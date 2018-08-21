aca94daa3db81e4bbb5f75b7882afc56611a2e86
BenjaminPMLovegood <benjaminpmlee@gmail.com>
2018-08-22 05:24:13 +0800

    improved docker image

--- c9docker/files/init.sh
+++ c9docker/files/init.sh
@@ -1,12 +1,49 @@
-echo 'root:fxckyourself' | chpasswd
+echo "loading root password..."
+chpasswd < /root/rootpasswd
+rm /root/rootpasswd
+
 echo -e "LANG=\"en_US.UTF-8\"" > /etc/default/local
 
+echo "adding user..."
+read username < /root/username
+read userpasswd < /root/userpasswd
+userdir="/home/$username"
+rm /root/username /root/userpasswd
+echo "user is ($username:$userpasswd)"
+mkdir $userdir
+useradd -d $userdir $username
+echo "$username:$userpasswd" | chpasswd
+chown $username $userdir
+
+echo "installing essential packages..."
 apt-get update
 apt-get upgrade -y
 
 apt-get install -y apt-utils
-apt-get install -y sudo nodejs npm git g++ curl tmux bash
+apt-get install -y sudo nodejs npm git g++ curl tmux 
 
-echo "registry = https://registry.npm.taobao.org" >> ~/.npmrc
+echo "writing entry script..."
+echo "PASS=\"username:password\""                > /root/start.sh
+echo "if [ -a /root/c9auth/password ]; then"    >> /root/start.sh
+echo "    echo \"password found. load it.\""    >> /root/start.sh
+echo "    read PASS < /root/c9auth/password"    >> /root/start.sh
+echo "fi"                                       >> /root/start.sh
+echo "sudo -i -u $username node $userdir/c9/server.js --listen 0.0.0.0 --port 8080 -a \$PASS -w /workspace/" >> /root/start.sh
 
-echo "cat /root/hint" >> /etc/profile
+echo "setting hints..."
+while read line
+do
+    echo "echo \"$line\"" >> /etc/profile
+done < /root/hint
+rm /root/hint
+
+echo "installing c9..."
+mkdir /workspace/
+chown $username /workspace/
+
+sudo -i -u $username bash -c "echo \"registry = https://registry.npm.taobao.org\" >> $userdir/.npmrc"
+mv /root/c9inst.sh $userdir/c9inst.sh
+sudo -i -u $username bash $userdir/c9inst.sh -d $userdir/c9/
+
+rm $userdir/c9inst.sh
+rm /root/init.sh
--- c9docker/files/start.sh
+++
@@ -1,8 +0,0 @@
-PASS="username:password"
-
-if [ -a /root/c9auth/password ]; then
-    echo "password found. load it."
-    read PASS < /root/c9auth/password
-fi
-
-node /root/c9/server.js --listen 0.0.0.0 --port 8080 -a $PASS -w /root/workspace
